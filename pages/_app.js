3a2af8407172365d5a960892d038ab830026e73a
Youil412 <youil19980412@gmail.com>
2024-07-10 12:05:45 +0700

    css migration fix

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../public/assets/css/bootstrap.min.css';
+import '../public/assets/css/magnific-popup.css';
+import '../public/assets/css/animate.min.css';
+import '../public/assets/css/fontawesome-all.min.css';
+import '../public/assets/css/slick.css';
+import '../public/assets/css/swiper-bundle.css';
+import '../public/assets/css/default.css';
+import '../public/assets/css/style.css';
+import '../public/assets/css/responsive.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
