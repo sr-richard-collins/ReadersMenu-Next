09e31fd556cfa7d3354881f5c8ce584b16576483
hollynight127626 <hollynight127626@gmail.com>
2024-07-11 08:33:19 +0300

    change categorypage

--- src/app/[category_type]/[name]/page.js
+++ src/app/[category_type]/[name]/page.js
@@ -17,41 +17,48 @@ export async function generateStaticParams() {
   }
 }
 
+async function fetchCategories() {
+  try {
+    const response = await fetch(`http://tnreaders.in/api/user/categories`);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    return [];
+  }
+}
+
+async function fetchSeoData(name) {
+  try {
+    const response = await fetch(`http://tnreaders.in/api/user/seoCategory?id=${name}`);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching SEO data:', error);
+    return {};
+  }
+}
+
+function findCategory(categories, name) {
+  if (!name || !categories) return null;
+
+  for (const category of categories) {
+    if (category.data_query === name) {
+      return category;
+    }
+    if (category.child) {
+      const subCategory = category.child.find((subCategory) => subCategory.data_query === name);
+      if (subCategory) {
+        return subCategory;
+      }
+    }
+  }
+  return null;
+}
+
 export default async function Category({ params }) {
   const { name } = params;
 
-  function findCategory(categories, name) {
-    if (!name || !categories) return null;
-
-    for (const category of categories) {
-      if (category.data_query === name) {
-        return category;
-      }
-      if (category.child) {
-        const subCategory = category.child.find((subCategory) => subCategory.data_query === name);
-        if (subCategory) {
-          return subCategory;
-        }
-      }
-    }
-    return null;
-  }
-
-  async function fetchSeoData(name) {
-    try {
-      const response = await fetch(`http://tnreaders.in/api/user/seoCategory?id=${name}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching SEO data:', error);
-      return {};
-    }
-  }
-
-  // Fetch categories synchronously within the function
-  const categoriesResponse = await fetch(`http://tnreaders.in/api/user/categories`);
-  const categories = await categoriesResponse.json();
-
+  // Fetch categories and metadata
+  const categories = await fetchCategories();
   const selectCategory = findCategory(categories, name);
   const metadata = await fetchSeoData(name);
 
